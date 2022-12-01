59885091afe63f979aea5efbf1ffe0e054840b92
Joe Klonowski <jklonowski@g.hmc.edu>
2022-11-30 18:47:05 -0600

    Add king/rook game state for testing.

--- tests/constants.py
+++ tests/constants.py
@@ -7,3 +7,10 @@ KINGS_AND_ONE_PAWN_BOARD.add_piece("black", "K", "e8")
 KINGS_AND_ONE_PAWN_BOARD.add_piece("white", "P", "e2")
 
 KINGS_AND_ONE_PAWN_GAME_STATE = GameState(KINGS_AND_ONE_PAWN_BOARD, "white")
+
+KINGS_AND_ONE_ROOK_BOARD = ChessBoard()
+KINGS_AND_ONE_ROOK_BOARD.add_piece("white", "K", "e1")
+KINGS_AND_ONE_ROOK_BOARD.add_piece("black", "K", "e8")
+KINGS_AND_ONE_ROOK_BOARD.add_piece("white", "R", "a1")
+
+KINGS_AND_ONE_ROOK_GAME_STATE = GameState(KINGS_AND_ONE_ROOK_BOARD, "white")

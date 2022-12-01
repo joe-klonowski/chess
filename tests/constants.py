38cde2fa52abc776f6d259f67289d3f0000efd1c
Joe Klonowski <jklonowski@g.hmc.edu>
2022-11-30 18:48:27 -0600

    Add kings and black pawn game state for testing.

--- tests/constants.py
+++ tests/constants.py
@@ -8,6 +8,13 @@ KINGS_AND_ONE_PAWN_BOARD.add_piece("white", "P", "e2")
 
 KINGS_AND_ONE_PAWN_GAME_STATE = GameState(KINGS_AND_ONE_PAWN_BOARD, "white")
 
+KINGS_AND_ONE_BLACK_PAWN_BOARD = ChessBoard()
+KINGS_AND_ONE_BLACK_PAWN_BOARD.add_piece("white", "K", "e1")
+KINGS_AND_ONE_BLACK_PAWN_BOARD.add_piece("black", "K", "e8")
+KINGS_AND_ONE_BLACK_PAWN_BOARD.add_piece("black", "P", "e7")
+
+KINGS_AND_ONE_BLACK_PAWN_GAME_STATE = GameState(KINGS_AND_ONE_BLACK_PAWN_BOARD, "black")
+
 KINGS_AND_ONE_ROOK_BOARD = ChessBoard()
 KINGS_AND_ONE_ROOK_BOARD.add_piece("white", "K", "e1")
 KINGS_AND_ONE_ROOK_BOARD.add_piece("black", "K", "e8")

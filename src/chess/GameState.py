19032c36592e21066842f97547816c35ed201b96
Joe Klonowski <jklonowski@g.hmc.edu>
2022-12-04 20:30:02 -0600

    Add checks on whether kings/rooks have moved in GameState.

--- src/chess/GameState.py
+++ src/chess/GameState.py
@@ -9,9 +9,12 @@ class GameState:
     player_to_move: str
 
     # Can be None or a square like "a5". For checking whether en passant is possible.
-    pawn_that_just_moved_two_squares: str
+    pawn_that_just_moved_two_squares: str = None
 
-    def __init__(self, board, player_to_move, pawn_that_just_moved_two_squares=None):
-        self.board = board
-        self.player_to_move = player_to_move
-        self.pawn_that_just_moved_two_squares = pawn_that_just_moved_two_squares
+    # The rest of these are for determining whether castling is possible.
+    white_king_has_moved: bool = False
+    black_king_has_moved: bool = False
+    a8_rook_has_moved: bool = False
+    a1_rook_has_moved: bool = False
+    h8_rook_has_moved: bool = False
+    h1_rook_has_moved: bool = False

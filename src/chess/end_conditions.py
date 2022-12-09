1cb9e0cf1a0891435ed0569c394adf8410252ddc
Joe Klonowski <jklonowski@g.hmc.edu>
2022-12-08 21:50:18 -0600

    Add is_stalemate function.

--- src/chess/end_conditions.py
+++ src/chess/end_conditions.py
@@ -3,3 +3,7 @@ from chess.get_possible_moves import is_king_in_check, get_possible_moves
 
 def is_checkmate(game_state) -> bool:
     return is_king_in_check(game_state) and get_possible_moves(game_state) == set()
+
+
+def is_stalemate(game_state) -> bool:
+    return not is_king_in_check(game_state) and get_possible_moves(game_state) == set()
--- tests/test_end_conditions.py
+++ tests/test_end_conditions.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 from chess.ChessBoard import ChessBoard
 from chess.GameState import GameState
 from chess.constants import STARTING_GAME_STATE
-from chess.end_conditions import is_checkmate
+from chess.end_conditions import is_checkmate, is_stalemate
 
 
 class TestEndConditions(TestCase):
@@ -20,3 +20,33 @@ class TestEndConditions(TestCase):
 
     def test_is_checkmate_false(self):
         self.assertFalse(is_checkmate(STARTING_GAME_STATE))
+
+    def test_is_checkmate_stalemated_position(self):
+        board = ChessBoard()
+        board.add_piece("black", "K", "a8")
+        board.add_piece("white", "K", "e6")
+        board.add_piece("white", "Q", "b6")
+        game_state = GameState(board, "black")
+
+        self.assertFalse(is_checkmate(game_state))
+
+    def test_is_stalemate_true(self):
+        board = ChessBoard()
+        board.add_piece("black", "K", "a8")
+        board.add_piece("white", "K", "e6")
+        board.add_piece("white", "Q", "b6")
+        game_state = GameState(board, "black")
+
+        self.assertTrue(is_stalemate(game_state))
+
+    def test_is_stalemate_false(self):
+        self.assertFalse(is_stalemate(STARTING_GAME_STATE))
+
+    def test_is_stalemate_checkmate_position(self):
+        board = ChessBoard()
+        board.add_piece("black", "K", "e8")
+        board.add_piece("white", "K", "e6")
+        board.add_piece("white", "Q", "e7")
+        game_state = GameState(board, "black")
+
+        self.assertFalse(is_stalemate(game_state))
